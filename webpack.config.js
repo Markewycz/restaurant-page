8087d10f6a83beffb9b0c373283b1af6a5e8bb4b
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-07-19 20:53:09 +0200

    Add assets/Design header

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,23 @@ module.exports = {
     static: "./dist",
   },
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
   },
 };
